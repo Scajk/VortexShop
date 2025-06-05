aa01117d7a496f5204daa7a62fe4087fef5f79a8
unknown <scaj@discente.ifpe.edu.br>
2025-06-05 18:27:44 -0300

    feat: add fix base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
  
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vortexshop/',
+  base: '/VortexShop/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
